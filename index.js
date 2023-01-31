4957e16c8c4fb18ba81474453fe10c4776e65797
Ellementul <ananev.ivan.ur@ya.ru>
2023-01-31 17:40:41 +0500

    fixed export

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('./src/Manager'),
   "addTaskEvent": require('./src/events/add_task_event'),
-  "updateListEvent": require('./src/events/update_list_event')
+  "updateListEvent": require('./src/events/update_list_event'),
+  "readyEvent": require('./src/events/all_members_ready_event')
 }
